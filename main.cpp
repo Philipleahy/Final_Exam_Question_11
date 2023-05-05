4b9ff08373f27310133a473fdb98ba8573f54c9d
Philipleahy <philip.leahy@HOTMAIL.COM>
2023-05-05 17:08:13 +0000

    Finalised Code
    Signed-off-by: Philipleahy <philip.leahy@HOTMAIL.COM>

--- main.cpp
+++ main.cpp
@@ -1,12 +1,27 @@
 #include "mbed.h"
 
-DigitalOut myled(LED1);
+DigitalIn Left(p13);//Joystick Left
+DigitalIn Right(p16);//Joystick Right
+
+DigitalOut Red(p23);//RGB LED Red
+DigitalOut Blue(p25);//RGB LED Blue
+
 
 int main() {
+
   while(1) {
-    myled = 1;
-    wait(0.2);
-    myled = 0;
-    wait(0.2);
-  }
+      if(Left == 1){
+          Red = 0;
+          Blue = 1;
+      }
+      else if(Right == 1){
+          Red = 1;
+          Blue = 0;
+      }
+      else if(Left == 0) + (Right == 0);{
+          Red = 1;
+          Blue = 1;
+      }
+
+}
 }
